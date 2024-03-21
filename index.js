dad09cb0eb0cb30b7c3e6e6f0639f98ae2308002
abdullatif <abdullatifjr002@gmail.com>
2024-03-22 00:09:30 +0300

    deleted js file

--- index.js
+++
@@ -1,8 +0,0 @@
-// var Contact = /** @class */ (function () {
-//     function Contact(name, email, phone) {
-//         this.name = name;
-//         this.email = email;
-//         this.phone = phone;
-//     }
-//     return Contact;
-// }());
