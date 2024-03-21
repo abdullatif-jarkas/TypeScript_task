519013d8cd736ca5570c6580433a3fab4ab1408b
abdullatif <abdullatifjr002@gmail.com>
2024-03-22 00:06:45 +0300

    upload task files

---
+++ index.js
@@ -0,0 +1,8 @@
+// var Contact = /** @class */ (function () {
+//     function Contact(name, email, phone) {
+//         this.name = name;
+//         this.email = email;
+//         this.phone = phone;
+//     }
+//     return Contact;
+// }());
